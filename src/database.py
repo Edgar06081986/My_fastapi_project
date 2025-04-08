0bfbd603f62273ff47e8cd77308efb650c9e47e2
axasuser <aa06081986aa@gmail.com>
2025-04-08 09:33:24 +0300

    delete old venv and install uv

--- src/database.py
+++ src/database.py
@@ -2,6 +2,7 @@ from typing import Optional
 from config import settings
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, and_, func, insert, select, text, update
 
 engine = create_async_engine(url=settings.DATABASE_URL_asyncpg,
     echo=True,)
@@ -15,3 +16,6 @@ async_engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg,
     echo=True,
 )
+
+
+async_session_factory = async_sessionmaker(async_engine)

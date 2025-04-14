7508d6851a246134eb9e5257c1f570d10914265f
Edgar06081986 <aa06081986aa@gmail.com>
2025-04-14 23:47:18 +0300

    create new alembic files

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 from src.config import settings
 from alembic import context
 from src.database import Base
-
+from src.models import *
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config

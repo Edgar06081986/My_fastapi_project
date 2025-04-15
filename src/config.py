602c144823101d2e65150224653d7b67a3594df3
Edgar06081986 <aa06081986aa@gmail.com>
2025-04-16 00:46:47 +0300

    install dotenv

--- src/config.py
+++ src/config.py
@@ -14,6 +14,6 @@ class Settings(BaseSettings):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
   
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env_db")
 
 settings = Settings()

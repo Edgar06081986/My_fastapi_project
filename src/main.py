565d21a0f2d67cfab074db775d844c6494693ada
axasuser <aa06081986aa@gmail.com>
2025-04-23 17:01:17 +0300

    создал новую миграцию

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 from src.api_v1.clients.end_client import router as client_router
 from src.models.base import Base
 from src.api_v1.orders.end_order import router as order_router
@@ -12,22 +12,35 @@ from src.config import yc_settings, settings
 from src.api_v1 import router as router_v1
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     async with db_helper.engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-#
-#     yield
-
-
 app = FastAPI()
-# Подключаем роутер с двумя разными префиксами (если они не совпадают)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Для разработки можно разрешить все origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(client_router, prefix=settings.api_v1_prefix)
+app.include_router(order_router, prefix=settings.api_v1_prefix)
+app.include_router(jeweler_router, prefix=settings.api_v1_prefix)
+
+# Дополнительный префикс только если нужен
 if settings.api_v1_prefix != yc_settings.api_v1_prefix_2:
-    app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
-    app.include_router(router=router_v1, prefix=yc_settings.api_v1_prefix_2)
-else:
-    # Если префиксы одинаковые, подключаем только один раз
-    app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+    app.include_router(client_router, prefix=yc_settings.api_v1_prefix_2)
+    app.include_router(order_router, prefix=yc_settings.api_v1_prefix_2)
+    app.include_router(jeweler_router, prefix=yc_settings.api_v1_prefix_2)
+
+# # Подключаем роутер с двумя разными префиксами (если они не совпадают)
+# if settings.api_v1_prefix != yc_settings.api_v1_prefix_2:
+#     app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+#     app.include_router(router=router_v1, prefix=yc_settings.api_v1_prefix_2)
+# else:
+#     # Если префиксы одинаковые, подключаем только один раз
+#     app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 
 if __name__ == "__main__":

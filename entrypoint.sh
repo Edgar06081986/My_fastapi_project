0a5e6e1e52a252b78ec50a79f43600bf3555bb7e
axasuser <aa06081986aa@gmail.com>
2025-04-30 16:40:05 +0300

    скоректировал по pep8 docker-compose и в entrypoint.sh добавил в upgrade фишку чтобы логи светились цветными цветами для удобства при CI/CD

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "⏳ Запуск миграций Alembic..."
-alembic upgrade head
+alembic upgrade head || { echo "❌ Миграции не применены"; exit 1; }
 
 echo "✅ Миграции применены. Запуск FastAPI..."
 exec uvicorn src.main:app --host 0.0.0.0 --port 8000

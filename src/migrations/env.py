161bb88c50dc808510806c93184bd5ab60a681f2
Edgar06081986 <aa06081986aa@gmail.com>
2025-05-03 01:00:44 +0300

    create_engine добавил вместо engine_config чтобы алембик слушал и не падал ошибкой при создании миграции из  за того что постгейнер ещё не поднялся .раньше алембик ини выполнялся а не config.py

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -1,5 +1,5 @@
 from logging.config import fileConfig
-
+from sqlalchemy import  create_engine
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -8,7 +8,6 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
 from src.config import settings
 
 from src.models.models import JewelersOrm,Workload,ClientsOrm,OrdersOrm,M2mJewelersClientsORM
@@ -62,9 +61,8 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+    connectable = create_engine(
+        settings.DATABASE_URL_asyncpg + "?async_fallback=True",
         poolclass=pool.NullPool,
     )
 

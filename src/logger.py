99475d43167d9d2a3fc969df6aa44b275e614240
Edgar06081986 <aa06081986aa@gmail.com>
2025-05-03 01:01:23 +0300

    добавил логирование проекта

---
+++ src/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os
+
+# Убедись, что папка logs существует
+os.makedirs("logs", exist_ok=True)
+
+# Настройка логирования
+logging.basicConfig(
+    filename="logs/app.log",  # путь относительно корня проекта
+    filemode='a',
+    format='%(asctime)s %(levelname)s: %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
